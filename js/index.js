eb8b61fff840b75ab339384fa73d9387c4ad3b46
cloudlastresort <cloudlastresort@gmail.com>
2021-06-23 11:46:19 +0530

    added media queries

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+
+gsap.config({
+    nullTargetWarn:false
+});
 // -------------------------SWIPER-------------------------------
 
 function swiper(){
@@ -99,4 +103,183 @@ $('.header').on('click', '.nav .menu li a', function(){
     $('.nav').fadeOut(1200);
     $('.hamburger').removeClass('is-active');
 
+})
+
+//--------------------------LIGHT GALLARY--------------------
+function lgGallary(){
+    $('.lg-gallary').lightGallery({
+        selector:'.gallary',
+        download:false,
+    })
+}
+//-----------------------------BARBAJS----------------------------
+function init(){
+    const loader = document.querySelector('.loader');
+    gsap.set(loader, {
+        scaleX:0,
+        rotation:10,
+        xPercent:-5,
+        yPercent:-50,
+        transformOrigin:'left center',
+        autoAlpha:1
+    });
+    function loaderIn(){
+        return gsap.fromTo(loader,{
+            rotation:10,
+            scaleX:0,
+            xPercent:-5
+        },
+        {
+            duration:.8,
+            xPercent:0,
+            scaleX:1,
+            rotation:0,
+            ease:"Circ.easeInOut",
+            transformOrigin:'left center'
+        })
+    }
+    function loaderAway(){
+        return gsap.to(loader,{
+            duration:.8,
+            scaleX:0,
+            xPercent:5,
+            rotation:-10,
+            transformOrigin:'right center',
+            ease:"Circ.easeInOut"
+        })
+    }
+    function contentAnimation(){
+        var tl = gsap.timeline();
+        tl.from("[class*='box-left']", {
+            duration:1,
+            translateY:100,
+            opacity: 0,
+        });
+        tl.from("[class*='right']", {
+            duration:1,
+            translateY:100,
+            opacity: 0
+        },
+        "-=.1"
+        );
+        tl.to(".box-img .img-cover", {
+            clipPath:"polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+            opacity:1,
+            ease:"Circ.easeInOut"
+        },
+        "-=1.2"
+        );
+        tl.to('.images .img-cover', {
+            clipPath:"polygon(0 0 , 100% 0, 100% 100%, 0% 100%)",
+            opacity: 1,
+            ease:"Circ.easeInOut"
+        },
+        "-=5"
+        )
+    }
+
+    function loaderSingle(mediaQuery){
+        TweenMax.to('.box-title, .arrows, .swiper-pagination', .5, {
+            opacity:0,
+            ease:Power2.easeInOut,
+            delay:-1
+        })
+        TweenMax.to('.box-img', .7, {
+            position:'absolute',
+            width:'100%',
+            height:'100%',
+            right:0,
+            ease:Power2.easeInOut,
+            delay:-0.5
+        });
+
+        if(mediaQuery.matches){
+            TweenMax.to('.box-title', .5, {
+                opacity: 0,
+                display: "none",
+                ease:Power2.easeInOut,
+                delay:.5
+            });
+            TweenMax.to(".content", .9,{
+                height: "30vh",
+                ease:Power2.easeInOut,
+                delay:.7
+            });
+        }else{
+            TweenMax.to('.content', .9,{
+                height: "calc(100vh - 100px)",
+                ease:Power2.easeInOut,
+                delay:.7
+            })
+        }
+    }
+    var mediaQuery = window.matchMedia("(max-width:991.98px)");
+    mediaQuery.addEventListener("change", () => {
+        this.checkNative();
+    })
+
+    barba.hooks.before(() => {
+        document.querySelector('html').classList.add('is-transitioning');
+    });
+    barba.hooks.after(() => {
+        document.querySelector('html').classList.remove('is-transitioning');
+        swiper();
+        lgGallary();
+    });
+
+    barba.hooks.enter(() => {
+        window.scrollTo(0,0);
+    });
+
+    function delay(n){
+        n = n || 2000;
+        return new Promise(done => {
+            setTimeout(() => {
+                done();
+            }, n);
+        });
+    }
+
+    barba.init({
+        sync:true,
+        transitions:[
+            {
+                name:'transitionDefault',
+                async leave(data){
+                    const done = this.async();
+                    await loaderIn();
+                    await delay(800);
+                    done();
+                },
+                async enter(data){
+                    loaderAway();
+                    contentAnimation();
+                }
+            },
+            {
+                name:"transitionSingle",
+                async leave(data){
+                    const done = this.async();
+                    await loaderSingle(mediaQuery);
+                    await delay(2000);
+                    done();
+                },
+                async enter(data){
+
+                },
+                from:{
+                    namespace:'home'
+                },
+                to:{
+                    namespace:'work'
+                }
+            }
+        ]
+      });
+}
+
+window.addEventListener('load', function(){
+    init();
+    swiper();
+    lgGallary();
 })
